9c0273bbd516f00703260a6666c573f02ce5b70a
KoPhaiDun <lamchidung373@gmail.com>
2023-08-27 22:02:24 +0700

    update

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
 import React from "react";
 import NavBar from "./components/NavBar";
+import Footer from "./components/Footer";
 
 function App() {
   return (
-    <div>
+    <div className="px-3">
       <NavBar />
+      <header className="flex justify-between mb-8">
+        <h3>Stays in Finland</h3>
+        <p>14+ Stays</p>
+      </header>
+      <Footer />
     </div>
   );
 }
--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,58 +1,67 @@
 import React from "react";
 import logo from "../assets/logo.svg";
 import { BiSearchAlt2 } from "react-icons/bi";
+import { RiCloseLine } from "react-icons/ri";
 
 function NavBar() {
   const styles = {
-    input: "px-2 py-1 border-r-2 focus:outline-none text-lg",
-    label: "border-none border-r-2 border-solid border-gray-200 ",
-    btn: "w-[53px] h-[55px] bg-white cursor-pointer text-2xl",
+    container:
+      "mx-auto w-4/5 shadow-md rounded-2xl flex items-center cursor-pointer w-max-content",
+    input: "relative px-5 py-2 border-r border-solid border-gray-300",
+    btn: "px-5 py-5 mx-auto text-red-500 font-black",
+    text: "text-gray-400 text-base",
+
+    modalHeader: "flex justify-between items-center mb-8",
+    modalContainer: "w-full relative",
+    searchContainer: "shadow-md rounded-2xl ",
   };
   return (
-    <nav>
+    <nav className="mt-5 mb-12 sm:flex justify-between items-center">
       <div>
-        <img src={logo} alt="img of the logo" className="mb-9" />
+        <img src={logo} alt="img of the logo" className="mb-8 " />
       </div>
-      {/* right */}
-      <div>
-        <form className="w-auto flex h-[55px] py-[10px] mx-9 rounded-2xl shadow-md">
-          <label className={styles.label}>
-            <p className="hidden">LOCATION</p>
-            <input
-              type="text"
-              placeholder="Add location"
-              className={styles.input}
-            />
-          </label>
-          <label className={styles.label}>
-            <p className="hidden">GUESTS</p>
-            <input
-              type="text"
-              placeholder="Add guests"
-              className={styles.input}
-            />
-          </label>
-          <button className={styles.btn}>
-            <BiSearchAlt2 />
-          </button>
-        </form>
 
-        <div className="">
-          <ul className="">
-            <li>
-              <option value="Helsinki, Finland">Helsinki, Finland</option>
-            </li>
-            <li>
-              <option value="Turku, Finland">Turku, Finland</option>
-            </li>
-            <li>
-              <option value="Oulu, Finland">Oulu, Finland</option>
-            </li>
-            <li>
-              <option value="Vaasa, Finland">Vaasa, Finland</option>
-            </li>
-          </ul>
+      <div className={styles.container}>
+        <div className={styles.input}>
+          <p className="hidden">LOCATION</p>
+          <span className={styles.text}>Choose a Location</span>
         </div>
+        <div className={styles.input}>
+          <p className="hidden">GUESTS</p>
+          <span className={styles.text}>Add guests</span>
+        </div>
+        <div className={styles.btn}>
+          <BiSearchAlt2 />
+        </div>
+      </div>
+
+      {/* modal */}
+      <div className={styles.modalContainer}>
+        <div className={styles.modalHeader}>
+          <h5 className="font-bold text-sm text-gray-700">Edit your search</h5>
+          <button className="text-lg cursor-pointer">
+            <RiCloseLine className="font-black" />
+          </button>
+        </div>
+
+        <div className={styles.searchContainer}>
+          <div>
+            <p>Location</p>
+            <span>Choose a Location</span>
+          </div>
+          <div>
+            <p>Guests</p>
+            <span>Add Guests</span>
+          </div>
+          <div>
+            <button>
+              <BiSearchAlt2 />
+              <span>Search</span>
+            </button>
+          </div>
+        </div>
+
+        <div></div>
       </div>
     </nav>
   );
